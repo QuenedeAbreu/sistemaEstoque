c06f8df78c17a96e6f02d68e7dc5c04ce8768ecf
Quenede Abreu <quenede.in@gmail.com>
2021-01-15 16:30:20 -0500

    atualização de title

--- src/Config.php
+++ src/Config.php
@@ -2,7 +2,7 @@
 namespace src;
 
 class Config {
-    const BASE_DIR = '/ibramvc/public';
+    const BASE_DIR = '/sistemaEstoque/public';
            
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
